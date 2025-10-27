272e17d3ce6712f845a9b2d1641911441b0b54a8
veenaskumar <venaskumar812@gmail.com>
2025-10-27 07:22:16 +0000

    key existance and array validation added

--- src/README.md
+++
@@ -1,1 +0,0 @@
-# Welcome to env schema checker
--- src/index.mjs
+++ src/index.mjs
@@ -1,20 +1,61 @@
 import dotenv from "dotenv"
 import path from "path"
 import { fileURLToPath } from "url";
+import fs from "fs"
 
 //TASK
-//1) for now i only consider the root dir env
-//2) future release need to consider the all env files across the project
 
-// construct absolute path of the .env file
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-const absolutePath = path.resolve(dirname,"../tests/.env")
 
-dotenv.config({
-        path:absolutePath
+
+const loadEnvAndSchema=(envPath,schemaPath)=>{
+    const __filePath = process.cwd()
+    const envPathDirectory = path.resolve(__filePath,envPath)
+    const envData = dotenv.config({path:envPathDirectory})
+
+    const schemaPathDirectory = path.resolve(__filePath,schemaPath)
+    const schemaData = JSON.parse(fs.readFileSync(schemaPathDirectory))
+    checkEnvAndSchema(envData.parsed,schemaData)
+}
+
+const checkEnvAndSchema=(envData,schemaData)=>{
+    console.log(envData)
+    let error=[]
+    for(const [key,type] of Object.entries(schemaData)){
+        let value = process.env[key]
+        console.log(value)
+        if(value===undefined){
+            error.push(`---> undefined value for ${key}`)
+        }
+        if(Array.isArray(type)){
+            if(!type.includes(value)){
+                error.push(`---> Not a correct value for ${key}`)
+            }
+        }
     }
-)
+    if(error.length==0){
+        console.log("The schema is verified successfully")
+    }
+    else{
+        console.log("Errors")
+        error.forEach((index)=>console.log(error))
+    }
+}
 
-console.log(process.env.PORT)
+const validateEnv=({envPath,schemaPath})=>{
+    try{
+        if(!envPath || !schemaPath){
+            throw new Error("Env Path and Schema path is mandatory")
+        }
+        loadEnvAndSchema(envPath,schemaPath)
+    }
+    catch(error){
+        console.log(error.message)
+    }
+}
+
+
+    
+    
+
+
 
